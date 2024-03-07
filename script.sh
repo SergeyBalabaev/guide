1837f5d918495b2480b32b79ddb2ce00ab15b5ad
SergeyBalabaev <62759846+SergeyBalabaev@users.noreply.github.com>
2024-03-07 21:45:25 +0300

    Update script.sh
    Add sed script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
+sudo su
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 chmod +x ubuntu-mainline-kernel.sh
-sudo mv ubuntu-mainline-kernel.sh /usr/local/bin/
-cd /usr/local/bin/
-sudo ubuntu-mainline-kernel.sh -i v6.2.0
-sudo nano /etc/default/grub
-#
-#
+mv ubuntu-mainline-kernel.sh /usr/local/bin/
+/usr/local/bin/ubuntu-mainline-kernel.sh -i v6.2.0
+sed 's/GRUB_DEFAULT=0/GRUB_DEFAULT="1>4"/' /etc/default/grub > file
+cp file /etc/default/grub
+rm file
 sudo update-grub
